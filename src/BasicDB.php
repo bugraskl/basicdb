a1d1655857585fbdfd3008bfcb960c8f89bf9879
Buğra <bugraskl@gmail.com>
2022-12-15 15:31:32 +0300

    Önbellekleme Sistemi Eklendi
    Önbellekleme metotları eklendi ve yardımcı DBCache sınıfı eklendi.

--- src/BasicDB.php
+++ src/BasicDB.php
@@ -15,33 +15,131 @@
  */
 class basicdb extends \PDO
 {
+    /**
+     * @var
+     */
     private $dbName;
+    /**
+     * @var
+     */
     private $type;
+    /**
+     * @var
+     */
     private $sql;
+    /**
+     * @var
+     */
     private $unionSql;
+    /**
+     * @var
+     */
     private $tableName;
+    /**
+     * @var
+     */
     private $where;
+    /**
+     * @var
+     */
     private $having;
+    /**
+     * @var
+     */
     private $grouped;
+    /**
+     * @var
+     */
     private $group_id;
+    /**
+     * @var
+     */
     private $join;
+    /**
+     * @var
+     */
     private $orderBy;
+    /**
+     * @var
+     */
     private $groupBy;
+    /**
+     * @var
+     */
     private $limit;
+    /**
+     * @var
+     */
     private $page;
+    /**
+     * @var
+     */
     private $totalRecord;
+    /**
+     * @var
+     */
     public $pageCount;
+    /**
+     * @var
+     */
     private $paginationLimit;
+    /**
+     * @var
+     */
     private $html;
+    /**
+     * @var bool
+     */
     public $debug = false;
-    public $paginationItem = '<li class="[active]">
-        <a href="[url]">[text]</a>
-    </li>';
+    /**
+     * @var string
+     */
+    public $paginationItem = '<li class="page-item [active]"><a class="page-link" href="[url]">[text]</a></li>';
+    /**
+     * @var string[]
+     */
     public $reference = [
         'NOW()'
     ];
+    /**
+     * @var DBCache
+     */
+    private $cache;
+    /**
+     * @var bool
+     */
+    public $status = false;
+    /**
+     * @var string
+     */
+    public $charset = 'utf8';
+    /**
+     * @var string
+     */
+    public $cache_dir = '_cache/db/';
+    /**
+     * @var string
+     */
+    public $ext = '.db_cache';
+    /**
+     * @var string
+     */
+    public $ttl = '3600';
 
-    public function __construct($host, $dbname, $username, $password, $charset = 'utf8')
+
+
+    /**
+     * @param $host
+     * @param $dbname
+     * @param $username
+     * @param $password
+     * @param string $charset
+     * @param string $cache_dir
+     * @param string $ttl
+     * @param string $ext
+     * @param bool $status
+     */
+    public function __construct($host, $dbname, $username, $password, string $charset, string $cache_dir, string $ttl, string $ext, bool $status)
     {
         try {
             parent::__construct('mysql:host=' . $host . ';dbname=' . $dbname, $username, $password);
@@ -50,11 +148,16 @@ class basicdb extends \PDO
             $this->query('SET NAMES ' . $charset);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            $this->cache = new DBCache($cache_dir, $ttl, $ext, $status);
         } catch (PDOException $e) {
             $this->showError($e);
         }
     }
 
+    /**
+     * @param $tableName
+     * @return $this
+     */
     public function from($tableName)
     {
         $this->sql = 'SELECT * FROM ' . $tableName;
@@ -62,12 +165,20 @@ class basicdb extends \PDO
         return $this;
     }
 
+    /**
+     * @param $columns
+     * @return $this
+     */
     public function select($columns)
     {
         $this->sql = str_replace(' * ', ' ' . $columns . ' ', $this->sql);
         return $this;
+
     }
 
+    /**
+     * @return $this
+     */
     public function union()
     {
         $this->type = 'union';
@@ -75,6 +186,10 @@ class basicdb extends \PDO
         return $this;
     }
 
+    /**
+     * @param Closure $fn
+     * @return $this
+     */
     public function group(Closure $fn)
     {
         static $group_id = 0;
@@ -85,6 +200,13 @@ class basicdb extends \PDO
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @param $mark
+     * @param $logical
+     * @return $this
+     */
     public function where($column, $value = '', $mark = '=', $logical = '&&')
     {
         $this->where[] = [
@@ -98,6 +220,13 @@ class basicdb extends \PDO
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @param $mark
+     * @param $logical
+     * @return $this
+     */
     public function having($column, $value = '', $mark = '=', $logical = '&&')
     {
         $this->having[] = [
@@ -111,75 +240,138 @@ class basicdb extends \PDO
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @param $mark
+     * @return $this
+     */
     public function or_where($column, $value, $mark = '=')
     {
         $this->where($column, $value, $mark, '||');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @param $mark
+     * @return $this
+     */
     public function or_having($column, $value, $mark = '=')
     {
         $this->having($column, $value, $mark, '||');
         return $this;
     }
 
+    /**
+     * @param $targetTable
+     * @param $joinSql
+     * @param $joinType
+     * @return $this
+     */
     public function join($targetTable, $joinSql, $joinType = 'inner')
     {
         $this->join[] = ' ' . strtoupper($joinType) . ' JOIN ' . $targetTable . ' ON ' . sprintf($joinSql, $targetTable, $this->tableName);
         return $this;
     }
 
+    /**
+     * @param $targetTable
+     * @param $joinSql
+     * @return $this
+     */
     public function leftJoin($targetTable, $joinSql)
     {
         $this->join($targetTable, $joinSql, 'left');
         return $this;
     }
 
+    /**
+     * @param $targetTable
+     * @param $joinSql
+     * @return $this
+     */
     public function rightJoin($targetTable, $joinSql)
     {
         $this->join($targetTable, $joinSql, 'right');
         return $this;
     }
 
+    /**
+     * @param $columnName
+     * @param $sort
+     * @return $this
+     */
     public function orderBy($columnName, $sort = 'ASC')
     {
         $this->orderBy = ' ORDER BY ' . $columnName . ' ' . $sort;
         return $this;
     }
 
+    /**
+     * @param $columnName
+     * @return $this
+     */
     public function groupBy($columnName)
     {
         $this->groupBy = ' GROUP BY ' . $columnName;
         return $this;
     }
 
+    /**
+     * @param $start
+     * @param $limit
+     * @return $this
+     */
     public function limit($start, $limit)
     {
         $this->limit = ' LIMIT ' . $start . ',' . $limit;
         return $this;
     }
 
+    /**
+     * @return array|false|mixed|void|null
+     */
     public function all()
     {
         try {
             $query = $this->generateQuery();
-            $result = $query->fetchAll(parent::FETCH_ASSOC);
+            $cacheKey = md5($this->sql);
+            if ($this->cache->exists($this->tableName, $cacheKey)) {
+                return $this->cache->get($this->tableName, $cacheKey);
+            }
+            $results = $query->fetchAll(parent::FETCH_ASSOC);
+            $this->cache->set($this->tableName, $cacheKey, $results);
+            return $results;
+
+        } catch (PDOException $e) {
+            $this->showError($e);
+        }
+    }
+
+    /**
+     * @return mixed|void|null
+     */
+    public function first()
+    {
+        try {
+            $query = $this->generateQuery();
+            $cacheKey = md5($this->sql);
+            if ($this->cache->exists($this->tableName, $cacheKey)) {
+                return $this->cache->get($this->tableName, $cacheKey);
+            }
+            $result = $query->fetch(parent::FETCH_ASSOC);
+            $this->cache->set($this->tableName, $cacheKey, $result);
             return $result;
         } catch (PDOException $e) {
             $this->showError($e);
         }
     }
 
-    public function first()
-    {
-        try {
-            $query = $this->generateQuery();
-            return $query->fetch(parent::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            $this->showError($e);
-        }
-    }
-
+    /**
+     * @return false|PDOStatement
+     */
     public function generateQuery()
     {
         if ($this->join) {
@@ -211,10 +403,14 @@ class basicdb extends \PDO
         return $query;
     }
 
+    /**
+     * @param $conditionType
+     * @return void
+     */
     private function get_where($conditionType = 'where')
     {
         if (
-        (is_array($this->{$conditionType}) && count($this->{$conditionType}) > 0)
+            (is_array($this->{$conditionType}) && count($this->{$conditionType}) > 0)
         ) {
             $whereClause = ' ' . ($conditionType == 'having' ? 'HAVING' : 'WHERE') . ' ';
             $arrs = $this->{$conditionType};
@@ -307,12 +503,22 @@ class basicdb extends \PDO
         }
     }
 
+    /**
+     * @param $tableName
+     * @return $this
+     */
     public function insert($tableName)
     {
         $this->sql = 'INSERT INTO ' . $tableName;
+        $this->cache->clear($tableName);
         return $this;
     }
 
+    /**
+     * @param $data
+     * @param $value
+     * @return bool|void
+     */
     public function set($data, $value = null)
     {
         try {
@@ -346,23 +552,39 @@ class basicdb extends \PDO
         }
     }
 
+    /**
+     * @return false|string
+     */
     public function lastId()
     {
         return $this->lastInsertId();
     }
 
+    /**
+     * @param $tableName
+     * @return $this
+     */
     public function update($tableName)
     {
         $this->sql = 'UPDATE ' . $tableName;
+        $this->cache->clear($tableName);
         return $this;
     }
 
+    /**
+     * @param $tableName
+     * @return $this
+     */
     public function delete($tableName)
     {
         $this->sql = 'DELETE FROM ' . $tableName;
+        $this->cache->clear($tableName);
         return $this;
     }
 
+    /**
+     * @return false|int|void
+     */
     public function done()
     {
         try {
@@ -375,6 +597,9 @@ class basicdb extends \PDO
         }
     }
 
+    /**
+     * @return mixed
+     */
     public function total()
     {
         if ($this->join) {
@@ -399,6 +624,12 @@ class basicdb extends \PDO
         return $query['total'];
     }
 
+    /**
+     * @param $totalRecord
+     * @param $paginationLimit
+     * @param $pageParamName
+     * @return array
+     */
     public function pagination($totalRecord, $paginationLimit, $pageParamName)
     {
         $this->paginationLimit = $paginationLimit;
@@ -412,6 +643,11 @@ class basicdb extends \PDO
         ];
     }
 
+    /**
+     * @param $url
+     * @param $class
+     * @return void
+     */
     public function showPagination($url, $class = 'active')
     {
         if ($this->totalRecord > $this->paginationLimit) {
@@ -428,16 +664,25 @@ class basicdb extends \PDO
         }
     }
 
+    /**
+     * @return int
+     */
     public function nextPage()
     {
         return ($this->page + 1 < $this->pageCount ? $this->page + 1 : $this->pageCount);
     }
 
+    /**
+     * @return int
+     */
     public function prevPage()
     {
         return ($this->page - 1 > 0 ? $this->page - 1 : 1);
     }
 
+    /**
+     * @return void
+     */
     public function getSqlString()
     {
         $this->get_where('where');
@@ -445,70 +690,129 @@ class basicdb extends \PDO
         return $this->errorTemplate($this->sql, __CLASS__ . ' SQL Sorgusu');
     }
 
+    /**
+     * @param $column
+     * @param $values
+     * @return $this
+     */
     public function between($column, $values = [])
     {
         $this->where($column, $values, 'BETWEEN');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $values
+     * @return $this
+     */
     public function notBetween($column, $values = [])
     {
         $this->where($column, $values, 'NOT BETWEEN');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function findInSet($column, $value)
     {
         $this->where($column, $value, 'FIND_IN_SET');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function findInSetReverse($column, $value)
     {
         $this->where($column, $value, 'FIND_IN_SET_REVERSE');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function in($column, $value)
     {
         $this->where($column, $value, 'IN');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function notIn($column, $value)
     {
         $this->where($column, $value, 'NOT IN');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function like($column, $value)
     {
         $this->where($column, $value, 'LIKE');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function notLike($column, $value)
     {
         $this->where($column, $value, 'NOT LIKE');
         return $this;
     }
 
+    /**
+     * @param $column
+     * @param $value
+     * @return $this
+     */
     public function soundex($column, $value)
     {
         $this->where($column, $value, 'SOUNDEX');
         return $this;
     }
 
+    /**
+     * @param $name
+     * @param $args
+     * @return void
+     */
     public function __call($name, $args)
     {
         die($name . '  metodu ' . __CLASS__ . ' sınıfı içinde bulunamadı.');
     }
 
+    /**
+     * @param PDOException $error
+     * @return void
+     */
     private function showError(PDOException $error)
     {
         $this->errorTemplate($error->getMessage());
     }
 
+    /**
+     * @param $errorMsg
+     * @param $title
+     * @return void
+     */
     private function errorTemplate($errorMsg, $title = null)
     {
         ?>
@@ -584,6 +888,180 @@ class basicdb extends \PDO
     public function setAutoIncrement($tableName, $ai = 1)
     {
         return $this->query("ALTER TABLE `{$tableName}` AUTO_INCREMENT = {$ai}")->fetch();
+    }
+
+    /**
+     * Önbellekleme işlemini devredışı bırak.
+     *
+     * @return basicdb
+     */
+    public function nocache()
+    {
+        $this->cache->setStatus(false);
+        return $this;
+    }
+
+    /**
+     * Önbellekleme işlemini devreye sok.
+     *
+     * @return basicdb
+     */
+    public function cache()
+    {
+        $this->cache->setStatus(true);
+        return $this;
+    }
+
+}
+
+/**
+ *
+ */
+class DBCache
+{
+    // Önbellek dosyasının yolunu ve geçerlilik süresini (saniye cinsinden) tutacak
+
+    /**
+     * @var string
+     */
+    protected string $cache_dir;
+    /**
+     * @var string
+     */
+    protected string $ttl;
+    /**
+     * @var string
+     */
+    protected string $ext;
+    /**
+     * @var bool
+     */
+    protected bool $status;
+
+
+    // Yapılandırıcı fonksiyon: önbellek dosyasının yolunu ve geçerlilik süresini alır
+
+
+    /**
+     * @param string $cache_dir
+     * @param string $ttl
+     * @param string $ext
+     * @param bool $status
+     */
+    public function __construct(string $cache_dir, string $ttl, string $ext, bool $status)
+    {
+        $this->cache_dir = $cache_dir;
+        $this->ttl = $ttl;
+        $this->ext = $ext;
+        $this->status = $status;
+        if ($this->status == false) {
+            return;
+        }
+        if (!is_dir($this->cache_dir)) {
+            // Klasör mevcut değil, oluşturulması gerekiyor
+            mkdir($this->cache_dir);
+        }
+    }
+
+    // Önbelleğe değer kaydeder
+
+    /**
+     * @param $filename
+     * @param $key
+     * @param $value
+     * @return void
+     */
+    public function set($filename, $key, $value)
+    {
+        if ($this->status == false) {
+            return;
+        }
+        $data = [];
+
+        if (file_exists($this->cache_dir . xrp($filename) . $this->ext)) {
+            $file_time = filemtime($this->cache_dir . xrp($filename) . $this->ext);
+
+            if ($file_time + $this->ttl >= time()) {
+                // Dosya hala geçerli, içeriğini oku ve $data değişkenine kaydet
+                $data = unserialize(file_get_contents($this->cache_dir . xrp($filename) . $this->ext));
+            } else {
+                $this->clear($filename);
+            }
+        }
+
+        $data[$key] = $value;
+
+        // Önbellek dosyasına değerleri kaydeder
+        file_put_contents($this->cache_dir . xrp($filename) . $this->ext, serialize($data));
+    }
+
+    // Önbellekten değer döndürür
+
+    /**
+     * @param $filename
+     * @param $key
+     * @return mixed|null
+     */
+    public function get($filename, $key)
+    {
+        if ($this->status == false) {
+            return;
+        }
+        $data = unserialize(file_get_contents($this->cache_dir . xrp($filename) . $this->ext));
+        return $data[$key] ?? null;
+    }
+
+    // Önbellekte bir anahtarın varlığını kontrol eder
+
+    /**
+     * @param $filename
+     * @param $key
+     * @return bool
+     */
+    public function exists($filename, $key)
+    {
+        if ($this->status == false) {
+            return;
+        }
+        if (file_exists($this->cache_dir . xrp($filename) . $this->ext)) {
+            $file_time = filemtime($this->cache_dir . xrp($filename) . $this->ext);
+
+            if ($file_time + $this->ttl >= time()) {
+                $data = unserialize(file_get_contents($this->cache_dir . xrp($filename) . $this->ext));
+                return isset($data[$key]);
+            }
+
+        }
+        return false;
+
+    }
+
+    // Önbelleği temizler
+
+    /**
+     * @param $filename
+     * @return void
+     */
+    public function clear($filename)
+    {
+        if ($this->status == false) {
+            return;
+        }
+        // Önbellek dosyasını siler
+        if (file_exists($this->cache_dir . xrp($filename) . $this->ext)) {
+            unlink($this->cache_dir . xrp($filename) . $this->ext);
+        }
+    }
+
+    /**
+     * Sets the status of the cache.
+     *
+     * @param bool $status
+     * @return void
+     */
+    public function setStatus(bool $status)
+    {
+        $this->status = $status;
     }
 
 }
